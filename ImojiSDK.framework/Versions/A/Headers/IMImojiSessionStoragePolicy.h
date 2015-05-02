09c7ccd4b1eed0093f193af0cb5dae6c4c1bda78
nkhoshini <nima@imojiapp.com>
2015-05-01 18:15:17 -0700

    allows for specifying storage paths for assets and persistent data

--- ImojiSDK.framework/Versions/A/Headers/IMImojiSessionStoragePolicy.h
+++ ImojiSDK.framework/Versions/A/Headers/IMImojiSessionStoragePolicy.h
@@ -16,4 +16,12 @@
 */
 + (instancetype)temporaryDiskStoragePolicy;
 
+/**
+*  @abstract Generates a storage policy that writes assets to a specified cache path and persistent information to
+*  persistentPath.
+*  @param cachePath URL path to store temporary files such as such as imoji images
+*  @param persistentPath URL path to store persistent information such as authentication information
+*/
++ (instancetype)storagePolicyWithCachePath:(NSURL *)cachePath persistentPath:(id)persistentPath;
+
 @end
